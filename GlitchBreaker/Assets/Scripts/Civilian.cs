7b4414d9c5eb48a2abd70327a7696a68f7b68d4d
unknown <cganders@mtu.edu>
2018-10-14 12:21:49 -0400

    Added basic civilian object and used raycast to have it detect the player

---
+++ GlitchBreaker/Assets/Scripts/Civilian.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Civilian : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.right, Mathf.Infinity);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.name == "Player")
+            {
+                print(hit.collider.gameObject.name);
+            }
+            
+        }
+
+
+	}
+}
