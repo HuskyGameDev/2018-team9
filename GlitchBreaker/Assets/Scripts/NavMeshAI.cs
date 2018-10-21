928adb0ede261cb3e76201d88b7462015ec9caba
unknown <nlfosdic@mtu.edu>
2018-10-21 14:03:54 -0400

    Added basic AI movement on top-down 3D scene

--- GlitchBreaker/Assets/Scripts/NavMeshAI.cs
+++ GlitchBreaker/Assets/Scripts/NavMeshAI.cs
@@ -7,14 +7,39 @@ public class NavMeshAI : MonoBehaviour {
 
 	public NavMeshAgent agent;
 	public Camera cam;
+	public float timer = 5;
+	private float timeCount;
 
+	void Start() {
+		agent.speed = 10f;
+
+	}
 	void Update() {
-     RaycastHit hit;
-     if (Input.GetMouseButtonDown(0)) {
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit))
-           agent.SetDestination(hit.point);
+     // RaycastHit hit;
+     // if (Input.GetMouseButtonDown(0)) {
+     //    Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+     //    if (Physics.Raycast(ray, out hit))
+     //       agent.SetDestination(hit.point);
+		 //
+     //    }
+		 //transform.parent.position = transform.position;
 
-        }
-    }
+		 timeCount += Time.deltaTime;
+		 if(timeCount >= timer)
+		 {
+			 if (timeCount >= timer) {
+				 Vector3 newPosition = RandomMove(transform.position, 25, -1);
+				 agent.SetDestination(newPosition);
+				 timeCount = 0;
+			 }
+		 }
+  }
+
+	static Vector3 RandomMove(Vector3 origin, float dist, int layermask) {
+			Vector3 randDirection = Random.insideUnitSphere * dist;
+			randDirection += origin;
+			NavMeshHit navHit;
+			NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+			return navHit.position;
+		}
 }
--- GlitchBreaker/Assets/Scripts/PlayerMovement.cs
+++ GlitchBreaker/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	private int speed = 75;
+	private int speed = 50;
 
 	// Use this for initialization
 	void Start () {
