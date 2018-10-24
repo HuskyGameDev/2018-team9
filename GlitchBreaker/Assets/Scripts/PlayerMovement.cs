45986fb6ae60b5c5c2e74d61d3a45235484ac0cc
unknown <nlfosdic@mtu.edu>
2018-10-24 11:38:55 -0400

    Improved player movement to flip sprite based on direction

--- GlitchBreaker/Assets/Scripts/PlayerMovement.cs
+++ GlitchBreaker/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	private int speed = 50;
-
+	private Vector3 scale;
+	private bool facingRight = false;
 	// Use this for initialization
 	void Start () {
 
@@ -17,6 +18,31 @@ public class PlayerMovement : MonoBehaviour {
 		//Get input from keyboard
   	var x = Input.GetAxisRaw("Horizontal") * 50f * Time.deltaTime;
     var y = Input.GetAxisRaw("Vertical") * 50f * Time.deltaTime;
+
+		//Determine if player is moving right and flip sprite
+		if (x > 0)
+		{
+			 //Flip sprite if it is not facing right already
+			 if (!facingRight)
+			 {
+				 scale = transform.localScale;
+			   scale.x = -1 * scale.x;
+			   transform.localScale = scale;
+				 facingRight = true;
+			 }
+		}
+		//Determine if player is moving left and flip sprite
+		else if (x < 0)
+		{
+			 //Flip sprite if it is  not facing left already
+			 if (facingRight)
+			 {
+				 scale = transform.localScale;
+			   scale.x = -1 * scale.x;
+			   transform.localScale = scale;
+				 facingRight = false;
+			 }
+		}
 
 		//Update player velocity
 		//Remove Transform.Translate because it ignores physics
