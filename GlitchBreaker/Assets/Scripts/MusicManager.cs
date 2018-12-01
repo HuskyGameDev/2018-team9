a5b6c85af923afa58da12e564582c616c8021cfe
Andrew Merdzinski <ammerdzi@mtu.edu>
2018-12-01 11:06:26 -0500

    Sounds and music implemented, no overlapping of music or repeating sounds.

---
+++ GlitchBreaker/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+     public AudioClip menu;
+	 public AudioSource music;
+
+	// Use this for initialization
+	void Start () {
+		music = this.gameObject.AddComponent<AudioSource>();
+        music.clip = menu;
+		music.Play();
+		music.loop = true;
+		DontDestroyOnLoad(music);
+	}
+	
+	// End any song based on environment
+	public void EndSong() {
+		music.Stop();
+	}
+	
+	// Start a new song based on level/menu
+	public void PlayNew(AudioClip newClip) {
+		music.clip = newClip;
+		music.Play();
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
